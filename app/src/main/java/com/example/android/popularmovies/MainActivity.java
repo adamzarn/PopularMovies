f6412267e53400a1ada0a927249070c9bea61834
Adam Zarn <ajzarn@gmail.com>
2017-06-09 18:08:53 -0500

    Second Commit

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.android.popularmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.res.AssetManager;
+import android.content.Context;
+import java.io.InputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Context context = getApplicationContext();
+        AssetManager am = context.getAssets();
+        try {
+            InputStream is = am.open("keys.txt");
+            Scanner s = new Scanner(is).useDelimiter("\\A");
+            String apiKey = s.hasNext() ? s.next() : "";
+            System.out.println(apiKey);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
